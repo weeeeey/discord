d3223c156043e351212727eb3d88910cab5433cb
zerojin <qser155@naver.com>
2023-08-28 16:54:54 +0900

    shadcn ui all push

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className="w-full h-full flex justify-center items-center ">
