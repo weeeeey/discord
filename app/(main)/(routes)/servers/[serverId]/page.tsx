518f92020a207fd9a8e62b119ad3bdc771f4e819
zerojin <qser155@naver.com>
2023-09-05 20:53:13 +0900

    ##

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,15 +1,52 @@
-import { currentUser } from '@clerk/nextjs';
-import React from 'react';
+import { redirectToSignIn } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+
+import currentProfile from '@/lib/current-profile';
+import { client } from '@/lib/prismadb';
 
 interface ServerIdPageProps {
     params: {
         serverId: string;
     };
 }
-const ServerIdPage = ({ params }: ServerIdPageProps) => {
-    const { serverId } = params;
 
-    return <div>{serverId}</div>;
+const ServerIdPage = async ({ params }: ServerIdPageProps) => {
+    const profile = await currentProfile();
+
+    if (!profile) {
+        return redirectToSignIn();
+    }
+
+    const server = await client.server.findUnique({
+        where: {
+            id: params.serverId,
+            members: {
+                some: {
+                    profileId: profile.id,
+                },
+            },
+        },
+        include: {
+            channels: {
+                where: {
+                    name: 'general',
+                },
+                orderBy: {
+                    createdAt: 'asc',
+                },
+            },
+        },
+    });
+
+    const initialChannel = server?.channels[0];
+
+    if (initialChannel?.name !== 'general') {
+        return null;
+    }
+
+    return redirect(
+        `/servers/${params.serverId}/channels/${initialChannel?.id}`
+    );
 };
 
 export default ServerIdPage;
